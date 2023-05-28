8d6576c1230ff1fbae4d559c91494a71f3f42d1c
Ashfaq147 <124809666+Ashfaq147@users.noreply.github.com>
2023-05-28 18:06:30 +0530

    Initial commit

---
+++ src/AboutPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Our Weather App is a comprehensive tool that provides accurate and up-to-date weather information for any location worldwide.
+        Whether you need to check the current conditions, view the forecast for the upcoming days, or plan your activities according to the weather,
+        our app has you covered.
+      </p>
+      <p>
+        With our Weather App, you can easily search for any city or use geolocation to automatically fetch weather data for your current location.
+        The app presents key weather parameters such as temperature, humidity, wind speed, and atmospheric pressure, giving you a clear overview
+        of the weather conditions at a glance.
+      </p>
+      <p>
+        Stay informed with our app's hourly and daily forecasts, which provide detailed insights into temperature variations, precipitation chances,
+        and wind patterns. Whether you're planning a trip, scheduling outdoor activities, or simply staying prepared, our app ensures you have the
+        necessary weather information to make informed decisions.
+      </p>
+      <p>
+        We prioritize user experience and strive to deliver a seamless and intuitive interface. Our Weather App is designed with a user-friendly
+        layout and visually appealing graphics to enhance your weather browsing experience.
+      </p>
+      <p>
+        Trust our Weather App to keep you updated with the latest weather conditions, wherever you are. Enjoy using our app and make the most
+        of your day, rain or shine!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,105 @@
-import logo from './logo.svg';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+function WeatherDetails({ weather }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="col-md-6 text-center">
+        <div className="shadow rounded wetherResultBox mt-3 ">
+        <img
+                className="weathorIcon"
+                src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+              />
+        <h2>Weather Details</h2>
+        <p>Temperature: {weather.main.temp}°C</p>
+        <p>Humidity: {weather.main.humidity}%</p>
+        <p>Wind Speed: {weather.wind.speed} m/s</p>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+function App() {
+  const apiKey = "c5337a8a266ea5c4feda5cd776718be1";
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    setLoading(true);
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    axios.get(apiURL)
+      .then((res) => {
+        console.log("response", res.data);
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("err", err);
+        setLoading(false);
+      });
+  };
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
+
+  return (
+    <div className="col-md-12">
+      <div className="wetherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input
+            type="text"
+            className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Search"}
+          </button>
+        </div>
+      </div>
+
+      {loading ? (
+        <div className="text-center mt-3">
+          <p>Loading...</p>
+        </div>
+      ) : Object.keys(data).length > 0 ? (
+        <div className="row">
+          <div className="col-md-6 text-center mt-5">
+            <div className="shadow rounded wetherResultBox">
+              <img
+                className="weathorIcon"
+                src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+              />
+              <h5 className="weathorCity">{data?.name}</h5>
+              <h6 className="weathorTemp">
+                {((data?.main?.temp) - 273.15).toFixed(2)}°C
+              </h6>
+            </div>
+          </div>
+          <div className="col-md-6 text-center pt-3">
+            <WeatherDetails weather={data} />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
